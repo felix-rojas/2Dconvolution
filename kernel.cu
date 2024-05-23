320cad70b398158c9e47029c2b71f329fcef7b37
felix-rojas <a01201946@tec.mx>
2024-05-23 07:50:28 -0600

    filter convolution

--- kernel.cu
+++ kernel.cu
@@ -1,122 +1,4 @@
 ﻿
-/*
-cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
-
-__global__ void addKernel(int *c, const int *a, const int *b)
-{
-    int i = threadIdx.x;
-    c[i] = a[i] + b[i];
-}
-
-int main()
-{
-    const int arraySize = 5;
-    const int a[arraySize] = { 1, 2, 3, 4, 5 };
-    const int b[arraySize] = { 10, 20, 30, 40, 50 };
-    int c[arraySize] = { 0 };
-
-    // Add vectors in parallel.
-    cudaError_t cudaStatus = addWithCuda(c, a, b, arraySize);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addWithCuda failed!");
-        return 1;
-    }
-
-    printf("{1,2,3,4,5} + {10,20,30,40,50} = {%d,%d,%d,%d,%d}\n",
-        c[0], c[1], c[2], c[3], c[4]);
-
-    // cudaDeviceReset must be called before exiting in order for profiling and
-    // tracing tools such as Nsight and Visual Profiler to show complete traces.
-    cudaStatus = cudaDeviceReset();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceReset failed!");
-        return 1;
-    }
-
-    return 0;
-}
-
-// Helper function for using CUDA to add vectors in parallel.
-cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size)
-{
-    int *dev_a = 0;
-    int *dev_b = 0;
-    int *dev_c = 0;
-    cudaError_t cudaStatus;
-
-    // Choose which GPU to run on, change this on a multi-GPU system.
-    cudaStatus = cudaSetDevice(0);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
-        goto Error;
-    }
-
-    // Allocate GPU buffers for three vectors (two input, one output)    .
-    cudaStatus = cudaMalloc((void**)&dev_c, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMalloc((void**)&dev_a, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMalloc((void**)&dev_b, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    // Copy input vectors from host memory to GPU buffers.
-    cudaStatus = cudaMemcpy(dev_a, a, size * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMemcpy(dev_b, b, size * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-    // Launch a kernel on the GPU with one thread for each element.
-    addKernel<<<1, size>>>(dev_c, dev_a, dev_b);
-
-    // Check for any errors launching the kernel
-    cudaStatus = cudaGetLastError();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
-        goto Error;
-    }
-    
-    // cudaDeviceSynchronize waits for the kernel to finish, and returns
-    // any errors encountered during the launch.
-    cudaStatus = cudaDeviceSynchronize();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
-        goto Error;
-    }
-
-    // Copy output vector from GPU buffer to host memory.
-    cudaStatus = cudaMemcpy(c, dev_c, size * sizeof(int), cudaMemcpyDeviceToHost);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-Error:
-    cudaFree(dev_c);
-    cudaFree(dev_a);
-    cudaFree(dev_b);
-    
-    return cudaStatus;
-}
-*/
-
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
@@ -124,27 +6,38 @@ Error:
 #include <fstream>
 #include <vector>
 
+#include <cassert>
+
+// Use (void) to silence unused warnings.
+#define assertm(exp, msg) assert(((void)msg, exp))
 
 // define the size of the image
 // assume square
 #define ROWS 1024
 #define COLS 1024
 
-// define threads per block 
-#define HILOS_POR_BLOQUE 512
+// constant for GPU mem
+#define FILTER_RADIUS 2
+__constant__ float FILTER[2 * FILTER_RADIUS + 1][2 * FILTER_RADIUS + 1]{1/9};
 
-__global__ void blurImage(float* input, float* output, size_t width, size_t height) {
+
+__global__ void blurImage(int* input, int* output, float* filter, int radius, size_t width, size_t height) {
   // by using a global index to the total size of the array
   // we get the position by using the threadIDx, blockIdx* block Dimx 
   // same goes for y index
-  size_t x_idx = threadIdx.x + blockIdx.x * blockDim.x;
-
-  // maybe dim 2 index? size_t y_idx = threadIdx.y + blockIdx.y * blockDim.y;
-  // the number of threads might not match so we need to guard that limit
-  if (x_idx < width) {
-    float sum = 0;
-    size_t count = 0;
+  int x_idx = threadIdx.x + blockIdx.x * blockDim.x;
+  int y_idx = threadIdx.y + blockIdx.y * blockDim.y;
+  float res = 0;
+    for (int filterRow = 0; filterRow < 2*radius+1; filterRow++) {
+      for (int filterCol = 0; filterCol < 2 * radius + 1; filterCol++) {
+        int inputRow = y_idx - radius + filterRow;
+        int inputCol = x_idx - radius + filterCol;
+        if (inputRow >= 0 && inputRow < height && inputCol >=0 && inputCol < width){
+          res += filter[filterRow][filterCol] * input[inputRow * width + inputCol];
+      }
+    }
   }
+  output[y_idx*width+x_idx] = res;
 }
 
 /* PGM format spec:
@@ -197,44 +90,55 @@ int main() {
     ROWS,
     std::vector<int>(COLS,255));
   drawDiag(data, 2);
-  writePGM("test.pgm", data);
-  //data.res
-  //bool img_file_created = writePGM("black_diagonal.pgm");
-  //if (img_file_created) {
-  //  int* h_a, * h_b, * h_c;
-  //  int* d_a, * d_b, * d_c;
+  bool img_file_created = writePGM("test.pgm", data);
+  assertm(img_file_created == true, "Image file created\n!");
 
-  //  //size_t arr_size = ROWS * sizeof(float);
+  int* h_input = new int[ROWS * COLS];
+  int* h_output = new int[ROWS * COLS];
 
-  //  cudaMalloc((void**)&d_a, arr_size);
-  //  cudaMalloc((void**)&d_b, arr_size);
-  //  cudaMalloc((void**)&d_c, arr_size);
+  const int filter_byte_size = sizeof(float) * (2 * FILTER_RADIUS + 1) * (2 * FILTER_RADIUS + 1);
+  const int input_byte_size = sizeof(int) * ROWS * COLS;
 
-  //  h_a = (int*)malloc(arr_size);
-  //  h_b = (int*)malloc(arr_size);
-  //  h_c = (int*)malloc(arr_size);
+  for (int i = 0; i < ROWS; i++) {
+    for (int j = 0; j < COLS; j++) {
+      h_input[i * COLS + j] = data[i][j];
+    }
+  }
 
+  // initialize normalizecd filter 
+  int* h_filter = new int[(2 * FILTER_RADIUS + 1) * (2 * FILTER_RADIUS + 1)] {1/9};
 
-  //  cudaMemcpy(d_a, h_a, arr_size, cudaMemcpyHostToDevice);
-  //  cudaMemcpy(d_b, h_b, arr_size, cudaMemcpyHostToDevice);
+  // GPU arrs
+  int* d_input;
+  int* d_output;
 
+  // gpu alloc
+  cudaMalloc(&d_input, input_byte_size);
+  cudaMalloc(&d_output, input_byte_size);
+  // inform data will NOT be changed
+  cudaMemcpyToSymbol(FILTER, h_filter, filter_byte_size);
 
-  //  //blurImage << < N / HILOS_POR_BLOQUE, HILOS_POR_BLOQUE >> > (d_a, d_b, d_c, N);
+  // Copy data from the host to the device
+  cudaMemcpy(d_input, h_input, input_byte_size, cudaMemcpyHostToDevice);
+  // might not need this
+  cudaMemcpy(FILTER, h_filter, filter_byte_size, cudaMemcpyHostToDevice);
 
-  //  cudaMemcpy(h_c, d_c, arr_size, cudaMemcpyDeviceToHost);
+  // Define block and grid dimensions
+  dim3 blockDim(16, 16);  // 16x16 threads per block
+  dim3 gridDim((COLS + blockDim.x - 1) / blockDim.x, (ROWS + blockDim.y - 1) / blockDim.y);
 
+  // Launch the kernel
+  blurImage << <gridDim, blockDim >> > (d_input, d_output, d_filter, FILTER_RADIUS, COLS, ROWS);
 
-  //  free(h_a);
-  //  free(h_b);
-  //  free(h_c);
-  //  cudaFree(d_a);
-  //  cudaFree(d_b);
-  //  cudaFree(d_c);
-  //  return 0;
-  //}
-  //else {
-  //  printf("Could not create file");
-  //  return 0;
-  //}
+  // Copy the result back to the host
+  cudaMemcpy(h_output, d_output, input_byte_size, cudaMemcpyDeviceToHost);
+
+  // Free device memory
+  cudaFree(d_input);
+  cudaFree(d_output);
+
+  delete[] h_input;
+  delete[] h_output;
+  delete[] h_filter;
   return 0;
 }
