08cfb46b4b2e02ce3f61ad1ebc994c03bbf87af6
felix-rojas <a01201946@tec.mx>
2024-05-23 10:38:57 -0600

    program is done

--- kernel.cu
+++ kernel.cu
@@ -7,6 +7,7 @@
 #include <vector>
 
 #include <cassert>
+#include <cmath>
 
 // Use (void) to silence unused warnings.
 #define assertm(exp, msg) assert(((void)msg, exp))
@@ -16,29 +17,40 @@
 #define ROWS 1024
 #define COLS 1024
 
+
+#define FILTER_SIZE 3
+#define FILTER_OFFSET (FILTER_SIZE/2);
+
 // constant for GPU mem
-#define FILTER_RADIUS 2
+__constant__ float FILTER[3 * 3];
 
-__constant__ float FILTER[2 * FILTER_RADIUS + 1][2 * FILTER_RADIUS + 1];
-
-
-__global__ void blurImage(int* input, float* output, float* filter, int radius, size_t width, size_t height) {
+__global__ void blurImage(float* input, float* output, float* filter, size_t width, size_t height) {
   // by using a global index to the total size of the array
   // we get the position by using the threadIDx, blockIdx* block Dimx 
   // same goes for y index
-  int x_idx = threadIdx.x + blockIdx.x * blockDim.x;
-  int y_idx = threadIdx.y + blockIdx.y * blockDim.y;
+  int row = threadIdx.x + blockIdx.x * blockDim.x;
+  int col = threadIdx.y + blockIdx.y * blockDim.y;
+  size_t N = width;
+
+  int row_offset = row - FILTER_OFFSET;
+  int col_offset = col - FILTER_OFFSET;
+  
   float res = 0.0f;
-    for (int filterRow = 0; filterRow < 2*radius+1; filterRow++) {
-      for (int filterCol = 0; filterCol < 2 * radius + 1; filterCol++) {
-        int inputRow = y_idx - radius + filterRow;
-        int inputCol = x_idx - radius + filterCol;
-        if (inputRow >= 0 && inputRow < height && inputCol >=0 && inputCol < width){
-          res += filter[(filterRow * 2 * radius + 1) + filterCol] * input[inputRow * width + inputCol];
+  // Iterate over all the rows
+  for (int i = 0; i < FILTER_SIZE; i++) {
+    // Go over each column
+    for (int j = 0; j < FILTER_SIZE; j++) {
+      if ((row_offset + i) >= 0 && (row_offset + i) < N) {
+        if ((col_offset + j) >= 0 && (col_offset + j) < N) {
+          // Accumulate result
+          res += input[(row_offset + i) * N + (col_offset + j)] *
+            FILTER[i * FILTER_SIZE + j];
+        }
       }
     }
   }
-  output[y_idx*width+x_idx] = res;
+  // Write back the result
+  output[row * N + col] = res;
 }
 
 /* PGM format spec:
@@ -66,7 +78,7 @@ bool writePGM(const std::string& filename, float* data, size_t rows, size_t cols
   // Write pixel values
   for (size_t x = 0; x < rows; x++) {
     for (size_t y = 0; y < cols; y++) {
-      file << data[x * cols + y] << " ";
+      file << std::round(data[x * cols + y]) << " ";
     }
     file << std::endl;
   }
@@ -83,41 +95,39 @@ void drawDiag(float* data, int rows, int cols, int width) {
 
     for (int j = start_col; j <= end_col; ++j) {
       // Set the value at the corresponding 1D array index
-      data[i * cols + j] = 0;
+      data[i * cols + j] = 1;
     }
   }
 }
 
-int main() {
-  // creates white image
-  float* data = new float[ROWS * COLS];
-  std::fill(data, data + ROWS * COLS, 255);
-  drawDiag(data, ROWS, COLS, 2);
-  bool img_file_created = writePGM("test.pgm", data, ROWS, COLS);
-  assertm(img_file_created == true, "Image file created\n!");
 
-  int* h_input = new int[ROWS * COLS];
+int main() {
+  int THREADS = 16;
+  // creates white image
+  float* h_input = new float[ROWS * COLS];
+  std::fill(h_input, h_input + ROWS * COLS, 255.0f);
+  drawDiag(h_input, ROWS, COLS, 2);
+
+  bool img_file_created = writePGM("test.pgm", h_input, ROWS, COLS);
+  assertm(img_file_created == true, "Image file created\n!");
+  
   float* h_output = new float[ROWS * COLS];
 
-  int filter_byte_size = sizeof(float) * (2 * FILTER_RADIUS + 1) * (2 * FILTER_RADIUS + 1);
-  int input_byte_size = sizeof(int) * ROWS * COLS;
+  std::fill(h_output, h_output + ROWS * COLS, 0.0f);
 
-  for (int i = 0; i < ROWS; i++) {
-    for (int j = 0; j < COLS; j++) {
-      h_input[i * COLS + j] = data[i * COLS + j];
-    }
-  }
+
+  int filter_byte_size = sizeof(float) * FILTER_SIZE * FILTER_SIZE;
+  int input_byte_size = sizeof(float) * ROWS * COLS;
+
+  float* h_filter = new float[FILTER_SIZE * FILTER_SIZE];
 
   // initialize normalizecd filter 
-  float* h_filter = new float[(2 * FILTER_RADIUS + 1)*(2 * FILTER_RADIUS + 1)];
-  
-  for (size_t i = 0; i < (2 * FILTER_RADIUS + 1) * (2 * FILTER_RADIUS + 1); i++) {
+  for (int i = 0; i < FILTER_SIZE * FILTER_SIZE; i++) {
     h_filter[i] = 1.0f / 9.0f;
   }
 
-
   // GPU arrs
-  int* d_input;
+  float* d_input;
   float* d_output;
 
   // gpu alloc
@@ -128,15 +138,18 @@ int main() {
 
   // Copy data from the host to the device
   cudaMemcpy(d_input, h_input, input_byte_size, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_output, h_output, input_byte_size, cudaMemcpyHostToDevice);
   // might not need this
   //cudaMemcpy(FILTER, h_filter, filter_byte_size, cudaMemcpyHostToDevice);
 
+  int block_size = (1024 + THREADS - 1) / THREADS;
+
   // Define block and grid dimensions
-  dim3 blockDim(16, 16);  // 16x16 threads per block
-  dim3 gridDim((COLS + blockDim.x - 1) / blockDim.x, (ROWS + blockDim.y - 1) / blockDim.y);
+  dim3 blockDim(THREADS, THREADS);  // 16x16 threads per block
+  dim3 gridDim(block_size, block_size);
 
   // Launch the kernel
-  blurImage <<<gridDim, blockDim>>>(d_input, d_output, h_filter, FILTER_RADIUS, COLS, ROWS);
+  blurImage <<<gridDim, blockDim>>>(d_input, d_output, FILTER, ROWS, COLS);
 
   // Copy the result back to the host
   cudaMemcpy(h_output, d_output, input_byte_size, cudaMemcpyDeviceToHost);
